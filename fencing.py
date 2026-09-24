e75965826a1ed7a2a8490bd0ecfacb59aaf6ae63
Miruthunjay <miruthun08@gmail.com>
2026-09-24 16:42:52 +0530

    fencing.py

---
+++ fencing.py
@@ -0,0 +1,8 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+a=int(input())
+b=int(input())
+area=2*(a+b)
+perimeter=(a*b)
+print("The required length is",area,"m")
+print("The required area of carpet is",perimeter,"sqm")
+
